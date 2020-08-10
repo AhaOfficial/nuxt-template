934c155abe30ddf2c558ba1afdfa1cc2d87281d3
hm-lee <hm.lee@a-ha.io>
2020-08-10 16:39:26 +0900

    Add vue-state-store

--- client/core/index.ts
+++ client/core/index.ts
@@ -1,3 +1,7 @@
 import * as VueAPI from '@vue/composition-api'
-import * as Vuex from 'vuex-module-decorators'
-export { VueAPI, Vuex }
+import { Store, store } from 'vue-state-store'
+export {
+    store,
+    Store,
+    VueAPI,
+}
--- client/store/index.ts
+++ client/store/index.ts
@@ -1,6 +1,1 @@
-import { Store } from 'vuex';
-import { init } from '~/core/store-accessor';
-
-const initializer = (store: Store<any>) => init(store);
-export const plugins = [initializer];
-export * from '~/core/store-accessor';
+export * from './vote'
