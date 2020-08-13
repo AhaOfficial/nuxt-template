3a37814d6c7f7ffc7b8cdeb90a80647f2e93fa39
hm-lee <hm.lee@a-ha.io>
2020-08-13 19:16:51 +0900

    테스트 코드 삭제

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
 		<!--
-        템플릿 태그에서 스토어 사용 시에도
-        타입스크립트 인텔리센스가 계속 지원됩니다.
-        -->
-		<!-- <h1>upVoteCount: {{ $vote.upVoteCount }}</h1>
-		<h1>downVoteCount: {{ $vote.downVoteCount }}</h1> -->
+		템플릿 태그에서 스토어 사용 시에도
+		타입스크립트 인텔리센스가 계속 지원됩니다.
+		-->
+		<h1>upVoteCount: {{ $vote.upVoteCount }}</h1>
+		<h1>downVoteCount: {{ $vote.downVoteCount }}</h1>
 		<Card />
 	</div>
 </template>
@@ -18,23 +18,13 @@ import { vote } from '~/store'
 export default VueAPI.defineComponent({
 	components: { Card },
 	setup(props, context) {
-		// 1초마다 추천을 발생시킵니다.
-		// (1초마다 템플릿에 값이 반영되는 것을 볼 수 있습니다.)
+		// * 1초마다 추천을 발생시킵니다.
+		// * (1초마다 템플릿에 값이 반영되는 것을 볼 수 있습니다.)
 		setInterval(() => vote.upVote(), 1000)
-		
-		console.log('check?')
-		const a = VueAPI.reactive({
-			foo: 1
-		})
-		VueAPI.watch(a, ()=> {
-			console.log('changed!', a)
-		})
-		VueAPI.set(a, 'bar', 1)
-		// console.log('it works?', a)
 
-		// 스토어 값을 자동으로 템플릿 태그에 바인딩합니다.
+		// * 스토어 값을 자동으로 템플릿 태그에 바인딩합니다.
 		return {
-			// $vote: vote.bind()
+			$vote: vote.bind()
 		}
 	}
 })
