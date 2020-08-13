7d008765d133a3a208e8c00b67e967a9803fc164
hm-lee <hm.lee@a-ha.io>
2020-08-13 16:51:18 +0900

    라이프사이클 예시 제거

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,24 +1,3 @@
 export * from './vote'
 export * from './todo'
 export * from './counter'
-
-import * as Lifecycle from 'nuxt-lifecycle'
-Lifecycle.onServerPrefetch(async url => {
-	console.log('onServerPrefetch start', url)
-	await (() => {
-		return new Promise(resolve => {
-			setTimeout(resolve, 2000)
-		})
-	})()
-	console.log('onServerPrefetch ended')
-})
-
-Lifecycle.onClientPrefetch(async url => {
-	console.log('onClientPrefetch start', url)
-	await (() => {
-		return new Promise(resolve => {
-			setTimeout(resolve, 2000)
-		})
-	})()
-	console.log('onClientPrefetch ended')
-})
