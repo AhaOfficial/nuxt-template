e07a90deefc4977282c7a5813bc0992475805175
hm-lee <hm.lee@a-ha.io>
2020-08-13 19:15:57 +0900

    nuxt-ssr-cache 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,6 +83,7 @@ const nuxtConfig: Config = {
 	modules: [
 		'@nuxtjs/axios',
 		'nuxt-lifecycle',
+		'nuxt-ssr-cache',
 	],
 
 	// * Nuxt 의 빌드 시 작동되는 모듈들을 지정합니다.
@@ -90,7 +91,7 @@ const nuxtConfig: Config = {
 		'@nuxt/typescript-build',
 		'@nuxtjs/tailwindcss',
 		'@nuxtjs/pwa',
-		"nuxt-compress"
+		"nuxt-compress",
 	],
 
 	// * Nuxt 의 기능을 확장할 플러그인들을 지정합니다.
@@ -99,6 +100,39 @@ const nuxtConfig: Config = {
 	// * 배포할 경로를 지정합니다.
 	srcDir: './client',
 
+
+	// * Nuxt-SSR-Cache 용 버전을 제공합니다.
+	version: cacheVersion,
+
+	// * Nuxt SSR 캐싱 정책을 명시합니다. https://github.com/arash16/nuxt-ssr-cache
+	cache: {
+		// * 여러 호스트를 동시에 운용하는 경우
+		// * req.hostname 과 같이 명시하거나 false
+		useHostPrefix: false,
+
+		// * 어떤 페이지들을 캐싱 대상으로 할지를 명시합니다.
+		pages: [
+			// * 모든페이지를 캐싱하려면: '/'
+			// * 정규식을 쓰려면: /^\/page3\/\d+$/
+			// * 최상위 경로만 캐싱하려면: /^\/$/
+			// * /page1 을 캐싱하려면: '/page1'
+
+			'/',
+		],
+
+		// * 캐시 저장 정책을 명시합니다.
+		store: {
+			type: 'memory',
+
+			// * 최대로 캐싱할 페이지 수를 기억합니다.
+			// * 최대에 도달하면 마지막에 사용된 페이지
+			// * 정보가 캐싱 해제됩니다. (LRU)
+			max: 200,
+
+			// * 몇 초 동안 보관할지를 명시합니다.
+			ttl: 2,
+		}
+	},
 
 	// * Nuxt 의 기본 로딩 컴포넌트의 로딩바 설정이 담깁니다.
 	// * https://vue-nuxt.gitbook.io/nuxt/configuration/loading
@@ -135,7 +169,8 @@ const nuxtConfig: Config = {
 		exposeConfig: false,
 	},
 
-	// * Brotli 압축을 적용합니다. (HTTPS 에 제한 적용)
+	// * Brotli 압축을 적용합니다.
+	// * (Brotli 는 HTTPS 상태에 한해서만 제한 적용됩니다.)
 	"nuxt-compress": {
 		gzip: {
 			cache: true
