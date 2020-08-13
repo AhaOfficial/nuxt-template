84a355e70066f5d251486a3d61ad6766452880ae
hm-lee <hm.lee@a-ha.io>
2020-08-13 14:32:37 +0900

    Add counter example

---
+++ client/store/counter.ts
@@ -0,0 +1,25 @@
+import { Store } from 'vue-state-store'
+
+export interface ICounter {
+    count: number
+    author: string
+}
+
+export class Counter extends Store<ICounter> {
+    value = this.bind()
+
+    async up() {
+        this.value.count++
+    }
+    async down() {
+        this.value.count--
+    }
+}
+
+export const useCount = () => {
+    const counter = new Counter({
+        author: '',
+        count: 0
+    })
+    return counter
+}
--- client/store/index.ts
+++ client/store/index.ts
@@ -1,1 +1,25 @@
-export * from './vote';
+export * from './vote'
+export * from './todo'
+export * from './counter'
+
+// import * as Lifecycle from 'nuxt-lifecycle'
+// Lifecycle.prefetchServer(async () => {
+//     console.log('prefetchServer start')
+//     await (() => {
+//         return new Promise((resolve) => {
+//             setTimeout(resolve, 2000)
+//         })
+//     })()
+//     console.log('prefetchServer ended')
+// })
+
+// Lifecycle.prefetchClient(async () => {
+//     console.log('prefetchClient start')
+//     await (() => {
+//         return new Promise((resolve) => {
+//             setTimeout(resolve, 2000)
+//         })
+//     })()
+//     console.log('prefetchClient ended')
+// })
+// Lifecycle.prefetch
