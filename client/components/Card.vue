6f13233bf9ee36b2993fb477f00e076b438684e6
hm-lee <hm.lee@a-ha.io>
2020-08-13 14:34:06 +0900

    Apply lint

--- client/components/Card.vue
+++ client/components/Card.vue
@@ -1,51 +1,56 @@
 <template>
-	<div class="p-4">
-		<div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
-			<img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
-			<div class="px-6 py-4 pb-2">
-				<div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-				<p class="text-gray-700 text-base">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-					nihil.
-				</p>
-			</div>
-			<div class="px-6 py-4">
-				<span class="badge mr-2">#photography</span>
-				<span class="badge mr-2">#여행이요.</span>
-				<span class="badge">#winter</span>
-			</div>
-		</div>
-	</div>
+    <div class="p-4">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+            <img
+                class="w-full"
+                src="https://tailwindcss.com/img/card-top.jpg"
+                alt="Sunset in the mountains"
+            />
+            <div class="px-6 py-4 pb-2">
+                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                    exercitationem praesentium nihil.
+                </p>
+            </div>
+            <div class="px-6 py-4">
+                <span class="badge mr-2">#photography</span>
+                <span class="badge mr-2">#여행이요.</span>
+                <span class="badge">#winter</span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import * as VueAPI from '@vue/composition-api';
-import { Service } from '~/core/index';
+    import * as VueAPI from '@vue/composition-api'
+    import { Service } from '~/core/index'
 
-const useAPI = async () => {
-	const resp = await Service.sample();
+    const useAPI = async () => {
+        const resp = await Service.sample()
 
-	console.log(resp);
+        // console.log(resp);
 
-	return resp;
-};
+        return resp
+    }
 
-export default VueAPI.defineComponent({
-	setup(props, context) {
-		// *
-		// sample api
+    export default VueAPI.defineComponent({
+        setup(props, context) {
+            // *
+            // sample api
 
-		useAPI();
-		return {};
-	}
-});
+            useAPI()
+            return {}
+        },
+    })
 </script>
 
 <style lang="postcss" scoped>
-.badge {
-	@apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
-	&:hover {
-		@apply bg-gray-300;
-	}
-}
+    .badge {
+        @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+        &:hover {
+            @apply bg-gray-300;
+        }
+    }
 </style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,31 +1,30 @@
 <template>
-	<div>
-		<!--
+    <div>
+        <!--
         템플릿 태그에서 스토어 사용 시에도
         타입스크립트 인텔리센스가 계속 지원됩니다.
         -->
-		<h1>upVoteCount: {{ $vote.upVoteCount }}</h1>
-		<h1>downVoteCount: {{ $vote.downVoteCount }}</h1>
-		<Card />
-	</div>
+        <h1>upVoteCount: {{ $vote.upVoteCount }}</h1>
+        <h1>downVoteCount: {{ $vote.downVoteCount }}</h1>
+        <Card />
+    </div>
 </template>
 
 <script lang="ts">
-import { VueAPI } from '~/core';
-import { Card } from '~/components';
-import { vote } from '~/store';
+    import { VueAPI } from '~/core'
+    import { Card } from '~/components'
+    import { vote } from '~/store'
+    export default VueAPI.defineComponent({
+        components: { Card },
+        setup(props, context) {
+            // 1초마다 추천을 발생시킵니다.
+            // (1초마다 템플릿에 값이 반영되는 것을 볼 수 있습니다.)
+            setInterval(() => vote.upVote(), 1000)
 
-export default VueAPI.defineComponent({
-	components: { Card },
-	setup(props, context) {
-		// 1초마다 추천을 발생시킵니다.
-		// (1초마다 템플릿에 값이 반영되는 것을 볼 수 있습니다.)
-		setInterval(() => vote.upVote(), 1000);
-
-		// 스토어 값을 자동으로 템플릿 태그에 바인딩합니다.
-		return {
-			$vote: vote.bind()
-		};
-	}
-});
+            // 스토어 값을 자동으로 템플릿 태그에 바인딩합니다.
+            return {
+                $vote: vote.bind(),
+            }
+        },
+    })
 </script>
