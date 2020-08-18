701f5dd999be8f88373761b8b31debbe3d273bf0
ChangMook-Lim <cm.lim@a-ha.io>
2020-08-18 12:44:30 +0900

    storybook 예제용 소스 &&  예제 추가 예정

--- client/components/task/Task.vue
+++ client/components/task/Task.vue
@@ -2,27 +2,35 @@
 <template>
   <div class="h-screen overflow-hidden p-10 flex justify-center">
     <div class="max-w-sm rounded overflow-hidden content-center shadow-lg">
-      <!-- <img src="https://tailwindcss.com/img/card-top.jpg" alt="testImg" class="w-full" /> -->
       <img src="~/assets/images/aha.svg" alt="testImg" class="w-full" />
       <div class="px-6 py-4">
         <input
+          v-model="todos"
           class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
           type="text"
           placeholder="todos"
         />
-        <button class="inline-block w-full my-5 bg-aha hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          class="inline-block w-full my-5 bg-aha hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          :class="isEmptyTodos && ['cursor-not-allowed', 'opacity-50']"
+          @click.prevent="addTodo"
+        >
           등록
         </button>
       </div>
       <div class="px-6 py-4 w-400">
-        <!-- opacity-50 -->
-        <div class="my-3 py-4 w-full border-solid border-t-2 border-b-2">
+        <div
+          v-for="(item, index) in todoList"
+          :key="index"
+          class="my-3 py-4 w-full border-solid border-t-2 border-b-2"
+          :class="item.done && 'opacity-50'"
+        >
           <span>1. 할일</span>
           <label class="inline-block flex justify-start items-start float-right">
             <div
               class="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500"
             >
-              <input type="checkbox" class="opacity-0 absolute" />
+              <input v-model="item.done" :value="item.done" type="checkbox" class="opacity-0 absolute" />
               <svg class="fill-current hidden w-4 h-4 text-teal-300 pointer-events-none" viewBox="0 0 20 20">
                 <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
               </svg>
@@ -53,18 +61,37 @@
 import { VueAPI } from '~/core'
 
 const useTodo = () => {
-  const todos = VueAPI.ref([])
-  const addTodo = todos.value.push({ idx: '', name: '', done: false })
+  const todos = VueAPI.ref('')
+  const todoList = VueAPI.ref([])
+  const isEmptyTodos = VueAPI.computed(() => todos.value === '')
 
-  return {}
+  const watchTodos = VueAPI.watch(
+    todoList,
+    (newValue, oldValue) => {
+      console.log(`value checkd :::::::::::::: > `, newValue)
+    },
+    { deep: true }
+  )
+
+  const addTodo = () => !isEmptyTodos.value && todoList.value.push({ idx: todoList.value.length + 1, name: todos.value, done: false })
+
+  return {
+    todos,
+    todoList,
+    isEmptyTodos,
+    addTodo
+  }
 }
 
 export default VueAPI.defineComponent({
   name: 'Task',
   props: {},
+  components: {},
 
   setup(props, context) {
-    return {}
+    return {
+      ...useTodo()
+    }
   }
 })
 </script>
