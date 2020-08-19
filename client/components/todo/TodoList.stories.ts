7680ea7bd08b8b019c82966978c7eab0568e5a6b
ChangMook-Lim <cm.lim@a-ha.io>
2020-08-19 18:19:08 +0900

    스토리북 예제 추가

--- client/components/todo/TodoList.stories.ts
+++ client/components/todo/TodoList.stories.ts
@@ -10,26 +10,60 @@ helper()
 
 export default {
   title: 'TodoList',
-  excludeStories: /.*Data$/
+  excludeStories: /.*Data$/,
+  decorators: [] // * 데코레이터는 <story>에 적용할 스타일을 지정합니다.
 }
 
-// export const dTodoData = [
-//   { ...todoData, id: '1', title: 'Task 1' },
-//   { ...todoData, id: '2', title: 'Task 2' },
-//   { ...todoData, id: '3', title: 'Task 3' },
-//   { ...todoData, id: '4', title: 'Task 4' },
-//   { ...todoData, id: '5', title: 'Task 5' },
-//   { ...todoData, id: '6', title: 'Task 6' }
-// ]
-// export const checkedTodoList = [...dTodoData.slice(0, 5), { id: '6', title: 'Task 6 (pinned)', done: true }]
+export const dTodoData = [
+  { ...todoData, idx: '1', name: 'Todo 1' },
+  { ...todoData, idx: '2', name: 'Todo 2' },
+  { ...todoData, idx: '3', name: 'Todo 3' },
+  { ...todoData, idx: '4', name: 'Todo 4' },
+  { ...todoData, idx: '5', name: 'Todo 5' },
+  { ...todoData, idx: '6', name: 'Todo 6' }
+]
+export const cTodoData = [...dTodoData.slice(0, 5), { idx: '6', name: 'Tod 6 Checked', done: true }]
 
-// export const Default = () => ({
-//   components: { TodoList },
-//   template: '<todo-list />',
-//   props: {
-//     todoMock: {
-//       default: () => dTodoData
-//     }
-//   },
-//   methods: actionsData
-// })
+export const Default = () => ({
+  components: { TodoList },
+  template: '<todo-list :todos="todos" @checkTodo="onCheckTodo"/>',
+  props: {
+    todos: {
+      default: () => dTodoData
+    }
+  },
+  methods: actionsData
+})
+
+export const CheckedTodo = () => ({
+  components: { TodoList },
+  template: '<todo-list :todos="todos" @checkTodo="onCheckTodo"/>',
+  props: {
+    todos: {
+      default: () => cTodoData
+    }
+  },
+  methods: actionsData
+})
+
+export const Loading = () => ({
+  components: { TodoList },
+  template: '<todo-list loading :todos="todos" @checkTodo="onCheckTodo"/>',
+  props: {
+    todos: {
+      default: () => cTodoData
+    }
+  },
+  methods: actionsData
+})
+
+export const Empty = () => ({
+  components: { TodoList },
+  template: '<todo-list @checkTodo="onCheckTodo"/>',
+  props: {
+    todos: {
+      default: () => cTodoData
+    }
+  },
+  methods: actionsData
+})
--- client/components/todo/TodoList.vue
+++ client/components/todo/TodoList.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="px-6 py-4 w-400">
-    <Todo :todo="todos" @checkTodo="onCheckTodo" />
+    <div v-if="isLoading">
+      <div v-for="(item, index) in todoList" :key="index" class="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto py-4 my-3">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-4 py-1">
+            <div class="space-y-2">
+              <div class="h-4 bg-gray-400 rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="isEmptyTodo">
+      <div class="border border-gray-300 rounded-md p-4 max-w-sm w-full mx-auto py-4 my-3 text-center">
+        <h1>🤔</h1>
+        Noting todo!!!
+      </div>
+    </div>
+    <div v-if="isTodoInit">
+      <Todo v-for="(todo, index) in todoList" :key="index" :todo="todo" @checkTodo="onCheckTodo" />
+    </div>
   </div>
 </template>
 
@@ -22,7 +41,14 @@ export default VueAPI.defineComponent({
   },
   components: { Todo },
 
-  setup(props, context) {}
+  setup(props, context) {
+    const onCheckTodo = () => context.emit('checkTodo', props.todos)
+    const isLoading = VueAPI.computed(() => props.loading)
+    const isEmptyTodo = VueAPI.computed(() => !props.loading && props.todos.length === 0)
+    const isTodoInit = VueAPI.computed(() => !props.loading && props.todos.length > 0)
+
+    return { todoList: props.todos, isEmptyTodo, isLoading, isTodoInit, onCheckTodo }
+  }
 })
 </script>
 
