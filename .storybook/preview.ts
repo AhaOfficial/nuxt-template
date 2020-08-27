62226e010bca627e6e3879f1c8393a8ecbfa497a
ChangMook-Lim <cm.lim@a-ha.io>
2020-08-27 10:54:57 +0900

    chore: helper 및 tailwindcss global 에서 세팅하게 변경

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,10 @@ import { addParameters } from '@storybook/vue'
 import { newViewport } from './assets/viewport'
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
+import { injectVueInstance } from '../client/core'
+import '../client/assets/css/tailwind.scss'
+injectVueInstance()
+
 addParameters({
   viewport: { viewports: newViewport },
   docs: {
