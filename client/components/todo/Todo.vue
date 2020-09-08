89d0efa63df9ed1a356e2b67758b5ba1bd7c91cc
ChangMook-Lim <cm.lim@a-ha.io>
2020-09-09 01:24:12 +0900

    build: port issue

--- client/components/todo/Todo.vue
+++ client/components/todo/Todo.vue
@@ -59,6 +59,7 @@
             class="border border-gray-300 rounded-md p-4 max-w-sm w-full mx-auto py-4 my-3 text-center"
           >
             <h1>🤔</h1>
+            <h1>🤔</h1>
             <h1>⚙️</h1>
             <h1>🤔</h1>
             <h1>⚙️</h1>
