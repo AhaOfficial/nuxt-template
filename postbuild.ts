38d197dafaa1974c21b2f59c7c81a1fb9ebb6a7c
hm-lee <hm.lee@a-ha.io>
2020-09-23 17:21:47 +0900

    build: postbuild 개선

--- postbuild.ts
+++ postbuild.ts
@@ -1,5 +1,3 @@
-import fs from 'fs'
-
 /**
  * 트랜스파일이 필요없는 모듈명의 정규식이 담깁니다.
  */
@@ -14,6 +12,7 @@ const ignoredTranspilePatterns = [
   'loader-utils',
 
   // * 뷰 & 넉스트
+  /^@vue.?/,
   /^@nuxt.?/,
   'nuxt'
 ]
@@ -64,64 +63,14 @@ const getDependencies = (ignoredTranspilePatterns: (string | RegExp)[]) => {
 }
 
 /**
- * 재귀 호출을 통해 주어진 경로에 하위폴더들의 목록을 얻어옵니다.
- * @param staticPath
- * @param subPath
- */
-const collectSubFolderList = (
-  staticPath: string,
-  subPath = '/',
-  ignoredTranspilePathPatterns: (string | RegExp)[]
-) => {
-  const files = fs.readdirSync(staticPath)
-  const folders = [{ subPath, staticPath }]
-
-  for (const file of files) {
-    const checkPath = staticPath + '/' + file
-    const stats = fs.statSync(checkPath)
-    if (!stats.isDirectory()) continue
-
-    // * 지양할 폴더경로 패턴과 일치하다면 배제합니다.
-    let isIgnored = false
-    for (const ignoreRegex of ignoredTranspilePathPatterns) {
-      if (new RegExp(ignoreRegex).test(checkPath)) {
-        isIgnored = true
-        break
-      }
-    }
-    if (isIgnored) continue
-
-    const collectedDatas = collectSubFolderList(
-      checkPath,
-      subPath + file + '/',
-      ignoredTranspilePathPatterns
-    )
-
-    for (const collectedData of collectedDatas) folders.push(collectedData)
-  }
-
-  return folders
-}
-
-/**
  * 빌드 후 바벨을 적용할 대상을 수집합니다.
  */
 export const postbuild = () => {
   /**
-   * 바벨을 적용할 모듈명이 담긴 목록입니다.
-   */
-  let moduleNamesOfNeedToBeTranspile: string[] = []
-  /**
    * 프로덕션에 사용되는 모듈 명 목록입니다.
    */
   const productionModuleNames = getDependencies(ignoredTranspilePatterns)
-
-  for (const productionModuleName of productionModuleNames) {
-    const packageJsonPath = `./node_modules/${productionModuleName}`
-    moduleNamesOfNeedToBeTranspile = [...moduleNamesOfNeedToBeTranspile]
-  }
-
-  return moduleNamesOfNeedToBeTranspile
+  return productionModuleNames
 }
 
 export default postbuild
